b85153cd78c94535e084c6b4fa9d151a517f9e7f
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-01-07 16:08:26 +0000

    wip user profiles

--- routes.js
+++ routes.js
@@ -5,3 +5,7 @@ Router.route("/", function(){
 Router.route("/vulnerability/new", function(){
   this.render("new-vulnerability-page");
 }, { name: "newVulnerability" });
+
+Router.route("/users/:_id", function(){
+  this.render("user-profile-page", {data: Meteor.users.findOne(this.params._id)});
+}, { name: "userProfile" });
