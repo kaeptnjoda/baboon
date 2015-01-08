4ed275c43a58150ce4febb8e7780e8708531b1b4
Gustav Simonsson <gustav.simonsson@gmail.com>
2015-01-08 15:18:11 +0100

    Add SMTP config usage at startup

--- server/startup.js
+++ server/startup.js
@@ -1,4 +1,11 @@
 Meteor.startup(function(){
+  process.env.MAIL_URL = 'smtp://' +
+    encodeURIComponent(Meteor.settings.smtp_username) + ':' +
+    encodeURIComponent(Meteor.settings.smtp_password) + '@' +
+    encodeURIComponent(Meteor.settings.smtp_host) + ':' +
+    Meteor.settings.smtp_port;
+  console.log(process.env.MAIL_URL);
+
   Accounts.emailTemplates.from = 'Ethereum Bounty Program <bounties@ethdev.com>';
   Accounts.emailTemplates.verifyEmail.subject = function(user) {
     return 'Confirm your email address for Ethereum Bounty Program';
