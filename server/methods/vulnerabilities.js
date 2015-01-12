03ae37c3775e00ee31676bc6e8a2171e9c9775e5
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-01-12 18:32:19 +0000

    defaulting max submissions properly

--- server/methods/vulnerabilities.js
+++ server/methods/vulnerabilities.js
@@ -5,8 +5,9 @@ Meteor.methods({
     if(id){
       //TODO: updates      
     }else{
-      if(Vulnerabilities.find({user: this.userId, accepted: false}).count() >=
-         (Meteor.settings.public.MAX_PENDING_SUBMISSIONS || 10)){
+      var max = ((Meteor.settings || {}).public || {}).MAX_PENDING_SUBMISSIONS || 10;
+      
+      if(Vulnerabilities.find({user: this.userId, accepted: false}).count() >= max){
         throw new Meteor.Error("max-submissions","Maximum number of pending submissions reached");
       }
       
